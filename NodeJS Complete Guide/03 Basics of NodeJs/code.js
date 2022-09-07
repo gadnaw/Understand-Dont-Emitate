207f3dc31e8e052ee1e70879163e133a400fb052
Peter Wandag <gadnaw@gmail.com>
2022-09-07 08:36:18 +0800

    Added Basic Functions of Web Server     1. Process a POST method     2. Stream and Buffer the data     3. Write a file using "fs"     4. Redirect the browser to other location

--- NodeJS Complete Guide/03 Basics of NodeJs/code.js
+++ NodeJS Complete Guide/03 Basics of NodeJs/code.js
@@ -1,22 +1,52 @@
 // Goal:
 // 1. Listen on port 8080
+
 // 2. Response to Home page
 // 3. Take the request
 // 4. Write file
 
+const { log } = require("console");
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
-  console.log(req.url, req.method);
   const url = req.url;
 
   if (url === "/") {
     res.write(
       '<html><head></head><body><form action="/message" method="POST"><input type="text" name="message0002"><button>Submit</button></form></body></html>'
     );
+    res.end();
   }
 
-  console.log(req);
+  const method = req.method;
+  const fs = require("fs");
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    // sample streem and buffer
+    req.on("data", function (chunk) {
+      body.push(chunk);
+    });
+    req.on("end", function () {
+      //Buffer is Built-in
+      console.log(Buffer.chunk);
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+
+      //Asynchronous write to file
+      fs.writeFile("message.txt", message, (err) => {
+        if (!err) {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+        }
+
+        return res.end();
+      });
+    });
+
+    //Using fs to write message to file
+
+    console.log(req.url, req.method);
+  }
 });
 
 server.listen(8080);
